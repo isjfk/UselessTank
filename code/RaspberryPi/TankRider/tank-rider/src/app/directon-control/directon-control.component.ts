ba5a9bd613602f11774c8f70de63e27c615c41bc
rachel.wang <rachel.wang02@sap.com>
2019-04-09 14:31:48 +0800

    Description: direction post command draft version and background image added

--- code/RaspberryPi/TankRider/tank-rider/src/app/directon-control/directon-control.component.ts
+++ code/RaspberryPi/TankRider/tank-rider/src/app/directon-control/directon-control.component.ts
@@ -16,14 +16,22 @@ export class DirectonControlComponent implements OnInit {
   ngOnInit() {
   }
 
-  //@HostBinding() public pressKey: boolean = false
+//@HostBinding() public pressKey: boolean = false
 
+/*
+command format $AP0:127X127Y!
+X larger than 127 turns right, less than 127 turns left
+Y larger than 127 goes forward, less than 127 goes back
+*/
 
   @HostListener('window:keydown', ['$event']) 
     pressEvent(event: any){
         if(event.keyCode === 38)  {
           let highlightup: HTMLElement = this.el.nativeElement.querySelector("#upbtn");
-          //this.tankcontrol.sendControl("http://10.128.167.223:8080/api/Port/v1/sendCommand");
+          this.tankcontrol.sendControl(JSON.stringify({
+            "command":"$AP0:127X130Y!",
+            "executionTime":"8000"
+        }));
           highlightup.style.backgroundColor = 'yellow';
           window.setTimeout(function() {
             highlightup.style.backgroundColor = 'white';
@@ -32,7 +40,10 @@ export class DirectonControlComponent implements OnInit {
 
         else if(event.keyCode === 40){
           let highlightdown: HTMLElement = this.el.nativeElement.querySelector("#downbtn");
-          //this.tankcontrol.sendControl("http://10.128.167.223:8080/api/Port/v1/sendCommand");
+          this.tankcontrol.sendControl(JSON.stringify({
+            "command":"$AP0:127X67Y!",
+            "executionTime":"8000"
+          }));
           highlightdown.style.backgroundColor = 'yellow';
           window.setTimeout(function() {
             highlightdown.style.backgroundColor = 'white';
@@ -41,7 +52,10 @@ export class DirectonControlComponent implements OnInit {
 
         else if(event.keyCode === 37){
           let highlightleft: HTMLElement = this.el.nativeElement.querySelector("#leftbtn");
-          //this.tankcontrol.sendControl("http://10.128.167.223:8080/api/Port/v1/sendCommand");
+          this.tankcontrol.sendControl(JSON.stringify({
+            "command":"$AP0:130X127Y!",
+            "executionTime":"8000"
+          }));
           highlightleft.style.backgroundColor = 'yellow';
           window.setTimeout(function() {
             highlightleft.style.backgroundColor = 'white';
@@ -50,7 +64,10 @@ export class DirectonControlComponent implements OnInit {
 
         else if(event.keyCode === 39){
           let highlightright: HTMLElement = this.el.nativeElement.querySelector("#rightbtn");
-          //this.tankcontrol.sendControl("http://10.128.167.223:8080/api/Port/v1/sendCommand");
+          this.tankcontrol.sendControl(JSON.stringify({
+            "command":"$AP0:67X127Y!",
+            "executionTime":"8000"
+          }));
           highlightright.style.backgroundColor = 'yellow';
           window.setTimeout(function() {
             highlightright.style.backgroundColor = 'white';
