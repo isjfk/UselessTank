aae2133ce3d731514b15eaf8caef57d5c02bcd11
Liu Linda <linda.liu01@sap.com>
2019-08-09 16:27:07 +0800

    make host dynamic

--- code/RaspberryPi/TankRider/tank-rider/src/app/camera/camera.component.ts
+++ code/RaspberryPi/TankRider/tank-rider/src/app/camera/camera.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from "@angular/platform-browser";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-camera',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./camera.component.less']
 })
 export class CameraComponent implements OnInit {
+  camera: { url: any };
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) {
+    this.camera = { url: "http://" + location.hostname + "/camera" };
+  }
+
+  secureUrl() {
+    console.log("cameraUrl:" + this.camera.url);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.camera.url);
+  }
 
   ngOnInit() {
   }
--- code/RaspberryPi/TankRider/tank-rider/src/service/abstractService.ts
+++ code/RaspberryPi/TankRider/tank-rider/src/service/abstractService.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import config from "../../config.json";
+import { Location } from '@angular/common';
 
 @Injectable({
     providedIn: 'root'
 })
 export abstract class AbstractService {
-    protected baseUrl = config.tank_service_url;
+
+    //protected baseUrl = config.tank_service_url;  
+    protected baseUrl = "http://" + location.hostname + ":8080";
 
     protected httpOptions = {
         headers: new HttpHeaders({
