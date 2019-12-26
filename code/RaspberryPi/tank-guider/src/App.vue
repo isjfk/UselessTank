41648ccb4762f8d318c70ef8be19b44d5087b04b
Liu,Linda <linda.liu01@sap.com>
2019-12-26 17:06:04 +0800

    add admin mode

--- code/RaspberryPi/tank-guider/src/App.vue
+++ code/RaspberryPi/tank-guider/src/App.vue
@@ -3,8 +3,11 @@
     <main>
       <router-view></router-view>
       <!-- img tag must be placed after previous div, otherwise the div will not overlapping over img -->
-      <img id="map" :src="mapUrl" alt="Oops... Where is my map?" />
+      <img id="map" :src="mapUrl" alt="Oops... Where is my map?" />  
     </main>
+    <div id="admin" :style="{ display:admin.display }">
+      <button id="setPostion" class="adminButton" @click="tankSetPosition($event)" :style="{ background:admin.setPosition.background }">Set Position</button>
+    </div>
   </div>
 </template>
 
@@ -14,7 +17,8 @@ export default {
 
   data () {
     return {
-      mapUrl: this.rosRestUrl() + '/map/image'
+      mapUrl: this.rosRestUrl() + '/map/image',
+      admin: {display: 'none', setPosition: {disabled: true, background: 'indianred'}}
     }
   },
 
@@ -30,8 +34,25 @@ export default {
       } else {
         return origin + ':5000'
       }
-    }
 
+      // return 'http://10.207.99.135:5000'
+    },
+    tankSetPosition (event) {
+      if (this.admin.setPosition.background === 'darkseagreen') {
+        this.admin.setPosition.background = 'indianred'
+        this.admin.setPosition.disabled = false
+      } else if (this.admin.setPosition.background === 'indianred') {
+        this.admin.setPosition.background = 'darkseagreen'
+        this.admin.setPosition.disabled = true
+      }
+    }
+  },
+
+  created () {
+    console.log('created')
+    if (window.location.pathname.includes('admin')) {
+      this.admin.display = 'flex'
+    }
   }
 
 }
@@ -62,6 +83,26 @@ export default {
   }
 
   #map {
-    height: 100%;
+    height: 100%;  
+  }
+
+  #admin{
+    position: absolute;
+    width: 250px;
+    height: 98%;
+    right: 0px;
+    align-items: center;
+    justify-content: center;
+    background: rgba(255,228,196,0.4);
+  }
+
+  .adminButton{
+    height: 40px;
+    width: 100%;
+    background: indianred;
+    font-size: x-large;
+    font-weight: bold;
+    color: ghostwhite;
+    text-align: center;
   }
 </style>
