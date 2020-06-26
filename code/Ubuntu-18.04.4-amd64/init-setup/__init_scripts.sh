40745419dc033dd38001351977d382acacc9ab39
Jimmy F. Klarke <isjfk@163.com>
2020-06-26 14:00:25 +0800

    Update Ubuntu install scripts.

--- code/Ubuntu-18.04.4-amd64/init-setup/__init_scripts.sh
+++ code/Ubuntu-18.04.4-amd64/init-setup/__init_scripts.sh
@@ -7,11 +7,14 @@ usermod -a -G dialout tank
 apt-get update
 
 # Don't promot for restart service while apt-install
-echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+#echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
 
 # Install sound packages
 apt-get install -y gstreamer1.0-plugins-base-apps
 apt-get install -y sox libsox-fmt-all
+
+# Configure bluetooth to support XBox One Controller
+echo 'options bluetooth disable_ertm=Y' >> /etc/modprobe.d/bluetooth.conf
 
 # Install common packages
 apt-get install -y vim
@@ -22,9 +25,10 @@ apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BAD
 apt-get update
 
 apt-get install -y ros-melodic-desktop-full
-apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+apt-get install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 rosdep init
 rosdep update
+#rosdep fix-permissions
 
 apt-get install -y ros-melodic-navigation ros-melodic-slam-gmapping ros-melodic-tf ros-melodic-laser-filters ros-melodic-robot-localization
 apt-get install -y ros-melodic-joy ros-melodic-teleop-twist-joy ros-melodic-teleop-twist-keyboard joystick
@@ -38,12 +42,15 @@ source devel/setup.bash
 
 # Install Python & Flask
 apt-get install -y python-pip
+# Fix pip SSLError issue
+python -m pip install --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org --upgrade pip
 pip install rospkg pyyaml
 pip install Flask Flask-Cors
 
 # Install NodeJS
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt-get install -y nodejs
+apt-get install -y npm
 
 # Create package directory
 mkdir -p ~/package
