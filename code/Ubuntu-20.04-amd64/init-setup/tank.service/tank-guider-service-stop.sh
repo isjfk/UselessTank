894398dd652dc7472d2e8d99ef96eaa3c89e342d
Jimmy F. Klarke <isjfk@163.com>
2020-08-17 22:13:12 +0800

    Add tank-guider service.

---
+++ code/Ubuntu-20.04-amd64/init-setup/tank.service/tank-guider-service-stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is called by tank.service to start tank ROS nodes on system startup.
+
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+TANK_DIR=$(cd ~; pwd)
+
+source $TANK_DIR/tank.bashrc
+
+killall node
