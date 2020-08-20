307047c291a90131395a4520f269362a6e757e72
Jimmy F. Klarke <isjfk@163.com>
2020-08-20 23:02:27 +0800

    Add slam_gmapping git package.

--- code/ROS/checkout3rdPackage.sh
+++ code/ROS/checkout3rdPackage.sh
@@ -4,24 +4,28 @@ SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 
 cd "$SCRIPT_DIR"
 rm -rf rplidar_ros
-git clone git@github.com:Slamtec/rplidar_ros.git
+git clone https://github.com/Slamtec/rplidar_ros.git
 
 cd "$SCRIPT_DIR"
 rm -rf realsense-ros
-git clone git@github.com:IntelRealSense/realsense-ros.git
+git clone https://github.com/IntelRealSense/realsense-ros.git
 cd realsense-ros/
 git checkout `git tag | sort -V | grep -P "^\d+\.\d+\.\d+" | tail -1`
 
 cd "$SCRIPT_DIR"
 rm -rf laser_filters
-git clone git@github.com:isjfk/laser_filters.git
+git clone https://github.com/isjfk/laser_filters.git
 cd laser_filters/
 git checkout laser_filter_sector
+
+cd "$SCRIPT_DIR"
+rm -rf slam_gmapping
+git clone https://github.com/ros-perception/slam_gmapping.git
 
 # Temprary packages
 cd "$SCRIPT_DIR"
 rm -rf joystick_drivers
-git clone git@github.com:ros-drivers/joystick_drivers.git
+git clone https://github.com/ros-drivers/joystick_drivers.git
 cd "$SCRIPT_DIR"
 rm -rf teleop_twist_joy
-git clone git@github.com:ros-teleop/teleop_twist_joy.git
+git clone https://github.com/ros-teleop/teleop_twist_joy.git
